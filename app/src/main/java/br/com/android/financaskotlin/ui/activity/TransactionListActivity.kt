eac9c4975e294c653bbf8bba79ec577a445c3ccd
Antonio Junio <a.junio.fc@gmail.com>
2018-04-03 01:59:56 -0300

    Implementar formulario do dialog

--- app/src/main/java/br/com/android/financaskotlin/ui/activity/TransactionListActivity.kt
+++ app/src/main/java/br/com/android/financaskotlin/ui/activity/TransactionListActivity.kt
@@ -1,11 +1,16 @@
 package br.com.android.financaskotlin.ui.activity
 
+import android.app.DatePickerDialog
+import android.content.DialogInterface
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.DatePicker
+import android.widget.Toast
 import br.com.android.financaskotlin.R
 import br.com.android.financaskotlin.enum.Type
 import br.com.android.financaskotlin.extension.formatToBrazilian
@@ -13,8 +18,10 @@ import br.com.android.financaskotlin.model.Transaction
 import br.com.android.financaskotlin.ui.ResumeView
 import br.com.android.financaskotlin.ui.adapter.TransactionAdapter
 import kotlinx.android.synthetic.main.activity_transaction_list.*
-import kotlinx.android.synthetic.main.card_resume.*
+import kotlinx.android.synthetic.main.transaction_form.view.*
 import java.math.BigDecimal
+import java.text.SimpleDateFormat
+import java.util.*
 
 class TransactionListActivity : AppCompatActivity() {
 
@@ -34,8 +41,49 @@ class TransactionListActivity : AppCompatActivity() {
                             window.decorView as ViewGroup,
                             false)
 
+                    val year = 2018
+                    val month = 3
+                    val day = 3
+
+                    val today = Calendar.getInstance()
+                    view.form_transacao_data.setText(today.formatToBrazilian())
+                    view.form_transacao_data.setOnClickListener {
+                        DatePickerDialog(this,
+                                DatePickerDialog.OnDateSetListener { v, year, month, day ->
+                                    val selectedDate = Calendar.getInstance()
+                                    selectedDate.set(year, month, day)
+                                    view.form_transacao_data.setText(selectedDate.formatToBrazilian())
+                                }, year, month, day).show()
+                    }
+
+                    val adapter = ArrayAdapter.createFromResource(this,
+                            R.array.income_categories, android.R.layout.simple_spinner_dropdown_item)
+                    view.form_transacao_categoria.adapter = adapter
+
                     AlertDialog.Builder(this)
-                            .setTitle(R.string.add_income).setView(view).show()
+                            .setTitle(R.string.add_income)
+                            .setView(view)
+                            .setPositiveButton("Adicionar",
+                                    { dialogInterface, i ->
+                                        val textValue = view.form_transacao_valor.text.toString()
+                                        val textDate = view.form_transacao_data.text.toString()
+                                        val textCategory = view.form_transacao_categoria.selectedItem.toString()
+
+                                        val value = BigDecimal(textValue)
+
+                                        val brazilianFormat = SimpleDateFormat("dd/MM/yyyy")
+                                        val convertDate = brazilianFormat.parse(textDate)
+                                        val date = Calendar.getInstance()
+                                        date.time = convertDate
+
+
+                                        val transaction = Transaction(type = Type.INCOME, value = value,
+                                                date = date, category = textCategory)
+
+                                        Toast.makeText(this, transaction.category,Toast.LENGTH_SHORT).show()
+                                    })
+                            .setNegativeButton("Cancelar", null)
+                            .show()
 
                 }
 
